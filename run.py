4e63e0a827ee262ceae83189a4b0ca3a5bca2a4e
TheECanyon <schindler89@aol.com>
2023-04-03 15:00:13 +0200

    Add parameter to enable early stopping, default behaviour is without early stopping. Add another parameter to set the amount of epochs after which early stopping can occurr.

--- run.py
+++ run.py
@@ -412,7 +412,8 @@ class Runner(object):
 				if kill_cnt % 10 == 0 and self.p.gamma > 5:
 					self.p.gamma -= 5 
 					self.logger.info('Gamma decay on saturation, updated value of gamma: {}'.format(self.p.gamma))
-				if kill_cnt > 501:
+
+				if self.p.enable_early_stopping & kill_cnt > self.p.early_stopping_limit:
 					self.logger.info("Early Stopping!!")
 					break
 
@@ -454,7 +455,8 @@ if __name__ == '__main__':
 	parser.add_argument('-gcn_drop',	dest='dropout', 	default=0.1,  	type=float,	help='Dropout to use in GCN Layer')
 	parser.add_argument('-hid_drop',  	dest='hid_drop', 	default=0.3,  	type=float,	help='Dropout after GCN')
 	parser.add_argument('-disable_gnn_encoder', dest='disable_gnn_encoder', default=False, type=bool, help='Disables the GNN encoder layer')
-
+	parser.add_argument('-enable_early_stopping', dest='enable_early_stopping', default=False, type=bool, help='Enables early stopping with after the amount of epochs that were set.')
+	parser.add_argument('-set_early_stopping_limit', dest='early_stopping_limit', default=25, type=int, help='Sets the amount of epochs after which early stopping can occurr.')
 	# ConvE specific hyperparameters
 	parser.add_argument('-hid_drop2',  	dest='hid_drop2', 	default=0.3,  	type=float,	help='ConvE: Hidden dropout')
 	parser.add_argument('-feat_drop', 	dest='feat_drop', 	default=0.3,  	type=float,	help='ConvE: Feature Dropout')
